985ea064a5ec226868c06ec0b5589bd40ef07b85
gtvdev <gustavobarrosfialho1402gb@gmail.com>
2025-01-02 17:45:03 -0300

    Segunda alteração

--- d035.py
+++ d035.py
@@ -1,8 +1,11 @@
 #desenvolva um programa que leia o commprimento de tres retas e diga ao usuario se elas podem ou nao formar um triangulo
-a = float(input('Informe o tamanho da base '))
-b = float(input('Informe o tamanho da lado1 '))
-c = float(input('Informe o tamanho do lado2 '))
-if a + b > c and a + c > b and b + c > a:
+print('-='*20)
+print('Analisador de Triângulos')
+print('-='*20)
+a = float(input('Informe o primeiro  segmento: '))
+b = float(input('Informe o segundo segmento:'))
+c = float(input('Informe o terceiro segmento:'))
+if a  < b + c and b < a + c and c < a + c : 
     print('é possível formar um triângulo')
 else : 
     print('Não é possível formar um triângulo')
